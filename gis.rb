f4ee032bd251a04be9d52f59da7799cf3ecddf88
unknown <pughbl@oregonstate.edu>
2022-12-06 16:17:39 -0800

    Final submission, updated Track and Waypoint to use args instead of numerous inputs.

--- gis.rb
+++ gis.rb
@@ -3,22 +3,23 @@
 class Track
   attr_accessor :name, :segments, :json
 
-  def initialize(segments, name='', json)
-    @name = name
-    @segments = segments
-    @json = json
+  def initialize(args)    
+    @segments = args[:segments]
+    @name = args[:name] || ''
+    @json = args[:json]
   end
 
   def get_json
     json.get_json(self)
   end
+
 end
 
 class TrackString
   def get_json(track)
     json = '{"type": "Feature", '
 
-    if track.name != ''
+    if !track.name.empty?
       json += '"properties": {"title": "' + track.name + '"},'
     end
 
@@ -34,13 +35,13 @@ class TrackString
       track_segment_json = ''
 
       segment.coordinates.each do |coordinate|
-        if track_segment_json != ''
+        if !track_segment_json.empty?
           track_segment_json += ','
         end
 
         track_segment_json += "[#{coordinate.lon},#{coordinate.lat}"
 
-        if coordinate.ele != nil
+        if coordinate.ele != 100000
           track_segment_json += ",#{coordinate.ele}"
         end
 
@@ -51,6 +52,7 @@ class TrackString
     end
     json + ']}}'
   end
+
 end
 
 class TrackSegment
@@ -65,7 +67,7 @@ end
 class Point
   attr_reader :lat, :lon, :ele
 
-  def initialize(lon, lat, ele=nil)
+  def initialize(lon, lat, ele=100000)
     @lon = lon
     @lat = lat
     @ele = ele
@@ -74,27 +76,28 @@ class Point
 end
 
 class Waypoint
-attr_accessor :lat, :lon, :ele, :name, :type, :json
+  attr_accessor :lat, :lon, :ele, :name, :type, :json
 
   # ele, name, and type are given contextually invalid default values
-  def initialize(lon, lat, ele=100000, name='', type='', json)
-    @lat = lat
-    @lon = lon
-    @ele = ele
-    @name = name
-    @type = type
-    @json = json
+  def initialize(args)
+    @lat = args[:lat]
+    @lon = args[:lon]
+    @ele = args[:ele] || 100000
+    @name = args[:name] || ''
+    @type = args[:type] || ''
+    @json = args[:json]
   end
 
   def get_json
     json.get_json(self)
   end
+
 end
 
 class WaypointString
   def get_json(waypoint)
     json = '{"type": "Feature","geometry": {"type": "Point","coordinates": '
-    json += "[#{waypoint.lon},#{waypoint.lat}"
+    json += "[#{waypoint.lat},#{waypoint.lon}"
 
     if waypoint.ele != 100000
       json += ",#{waypoint.ele}"
@@ -121,6 +124,7 @@ class WaypointString
     end
     json += "}"
   end
+
 end
 
 class World
@@ -134,7 +138,7 @@ class World
     @features.append(type)
   end
 
-  def to_geojson(indent=0)
+  def to_geojson
     string = '{"type": "FeatureCollection","features": ['
 
     @features.each_with_index do |feature, index|
@@ -148,10 +152,10 @@ class World
 
 end
 
-def main()
+def main
   json = WaypointString.new
-  w = Waypoint.new(-121.5, 45.5, 30, "home", "flag", json)
-  w2 = Waypoint.new(-121.5, 45.6, 100000, "store", "dot", json)
+  w = Waypoint.new(:lat => -121.5, :lon => 45.5, :ele => 30, :name => "home", :type => "flag", :json => json)
+  w2 = Waypoint.new(:lat => -121.5, :lon => 45.6, :ele => 100000, :name => "store", :type => "dot", :json => json)
   
   ts1 = [
   Point.new(-122, 45),
@@ -174,15 +178,14 @@ def main()
   track_segment3 = TrackSegment.new(ts3)
   json = TrackString.new
 
-  t = Track.new([track_segment1, track_segment2], "track 1", json)
-  t2 = Track.new([track_segment3], "track 2", json)
+  t = Track.new(:segments => [track_segment1, track_segment2], :name => "track 1", :json => json)
+  t2 = Track.new(:segments => [track_segment3], :name => "track 2", :json => json)
 
   world = World.new("My Data", [w, w2, t, t2])
 
-  puts world.to_geojson()
+  puts world.to_geojson
 end
 
 if File.identical?(__FILE__, $0)
-  main()
+  main
 end
-
--- tc_gis.rb
+++ tc_gis.rb
@@ -6,17 +6,17 @@ class TestGis < Test::Unit::TestCase
 
   def test_waypoints
     json = WaypointString.new
-    w = Waypoint.new(-121.5, 45.5, 30, "home", "flag", json)
+    w = Waypoint.new(:lat => -121.5, :lon => 45.5, :ele => 30, :name => "home", :type => "flag", :json => json)
     expected = JSON.parse('{"type": "Feature","properties": {"title": "home","icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5,30]}}')
     result = JSON.parse(w.get_json)
     assert_equal(result, expected)
 
-    w = Waypoint.new(-121.5, 45.5, 100000, '', "flag", json)
+    w = Waypoint.new(:lat => -121.5, :lon => 45.5, :ele => 100000, :name => '', :type => "flag", :json => json)
     expected = JSON.parse('{"type": "Feature","properties": {"icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
     result = JSON.parse(w.get_json)
     assert_equal(result, expected)
 
-    w = Waypoint.new(-121.5, 45.5, 100000, "store", '', json)
+    w = Waypoint.new(:lat => -121.5, :lon => 45.5, :ele => 100000, :name => "store", :type => '', :json => json)
     expected = JSON.parse('{"type": "Feature","properties": {"title": "store"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
     result = JSON.parse(w.get_json)
     assert_equal(result, expected)
@@ -41,12 +41,12 @@ class TestGis < Test::Unit::TestCase
     track_segment3 = TrackSegment.new(ts3)
     json = TrackString.new
 
-    t = Track.new([track_segment1, track_segment2], "track 1", json)
+    t = Track.new(:segments => [track_segment1, track_segment2], :name => "track 1", :json => json)
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}}')
     result = JSON.parse(t.get_json)
     assert_equal(expected, result)
 
-    t = Track.new([track_segment3], "track 2", json)
+    t = Track.new(:segments => [track_segment3], :name => "track 2", :json => json)
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 2"},"geometry": {"type": "MultiLineString","coordinates": [[[-121,45.5],[-122,45.5]]]}}')
     result = JSON.parse(t.get_json)
     assert_equal(expected, result)
@@ -54,8 +54,8 @@ class TestGis < Test::Unit::TestCase
 
   def test_world
     json = WaypointString.new
-    w = Waypoint.new(-121.5, 45.5, 30, "home", "flag", json)
-    w2 = Waypoint.new(-121.5, 45.6, 100000, "store", "dot", json)
+    w = Waypoint.new(:lat => -121.5, :lon => 45.5, :ele => 30, :name => "home", :type => "flag", :json => json)
+    w2 = Waypoint.new(:lat => -121.5, :lon => 45.6, :ele => 100000, :name => "store", :type => "dot", :json => json)
     ts1 = [
       Point.new(-122, 45),
       Point.new(-122, 46),
@@ -74,8 +74,8 @@ class TestGis < Test::Unit::TestCase
     track_segment3 = TrackSegment.new(ts3)
     json = TrackString.new
 
-    t = Track.new([track_segment1, track_segment2], "track 1", json)
-    t2 = Track.new([track_segment3], "track 2", json)
+    t = Track.new(:segments => [track_segment1, track_segment2], :name => "track 1", :json => json)
+    t2 = Track.new(:segments => [track_segment3], :name => "track 2", :json => json)
 
     w = World.new("My Data", [w, w2, t, t2])
 
